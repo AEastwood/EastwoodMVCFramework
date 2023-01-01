8e496328eb101cf2dcabc2e88bf5f395dd758392
Adam Eastwood <adam-3@live.co.uk>
2023-01-01 21:47:22 +0000

    Removed MobLike

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,4 +5,3 @@ use MVC\Classes\Routes\Router as Route;
 
 # GET routes
 Route::get('', [DefaultController::class, 'index'])->name('index');
-Route::get('moblike', [DefaultController::class, 'mobLike'])->name('moblike');
